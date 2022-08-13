76694c8cc32250afe43842fceef1305cf81d6eb2
Mori-Atsushi <at.sushi.tb@gmail.com>
2022-08-14 02:10:32 +0900

    Setup for ksp procesor tests

--- launcher-processor/build.gradle.kts
+++ launcher-processor/build.gradle.kts
@@ -15,4 +15,9 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("com.google.devtools.ksp:symbol-processing-api:1.7.10-1.0.6")
     implementation(project(":launcher-annotations"))
+
+    testImplementation(kotlin("test"))
+    testImplementation("com.google.truth:truth:1.1.3")
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.4.9")
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing-ksp:1.4.9")
 }
