a99f846d463cc32cfe66fee620b4a88aff1f6dd0
Mori-Atsushi <at.sushi.tb@gmail.com>
2022-08-13 21:25:44 +0900

    Setup ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,7 @@ plugins {
     id("com.android.library") version "7.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.7.10" apply false
 }
+
+subprojects {
+    apply(from = "$rootDir/config/ktlint.gradle.kts")
+}
