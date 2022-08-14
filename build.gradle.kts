7494e48aed7bdadc96d1d279519b89522e5f3011
Mori-Atsushi <at.sushi.tb@gmail.com>
2022-08-14 13:35:37 +0900

    Setup for publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,9 @@ plugins {
     id("com.android.application") version "7.2.2" apply false
     id("com.android.library") version "7.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.7.10" apply false
-    id("com.google.devtools.ksp") version "1.7.10-1.0.6" apply false
     id("org.jetbrains.kotlin.jvm") version "1.7.10" apply false
+    id("com.google.devtools.ksp") version "1.7.10-1.0.6" apply false
+    id("com.vanniktech.maven.publish") version "0.21.0"
 }
 
 subprojects {
